d4db2a0dc52a89b65045c27897a930332b60b4d9
Michael Lustenberger <mic@inofix.ch>
2017-08-22 13:42:26 +0200

    Timeout
    Add the timeout for the first test.

--- testlogger.ino
+++ testlogger.ino
@@ -1,6 +1,20 @@
+
+// wait a second (or so..)
+int timeout = 1000;
+
 // install Photoresistor on A0
 int lightPin = A0;
+
+
+
+
+
+// initialize the light variable
 int lightValue = 0;
+
+
+
+
 
 void setup() {
     Serial.begin(9600);
@@ -13,5 +27,7 @@ void loop() {
     Serial.print("Light Value:");
     Serial.print(lightValue);
     Serial.println(";");
+
+    delay(timeout);
 }
 
