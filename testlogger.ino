22c3c6af6d94f369c0719f0d8748027f70f51224
Michael Lustenberger <mic@inofix.ch>
2017-08-22 13:50:46 +0200

    Exact Temperature
    Add a temperature sensor.

--- testlogger.ino
+++ testlogger.ino
@@ -1,33 +1,53 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 
 // wait a second (or so..)
-int timeout = 1000;
+int timeout = 5000;
 
-// install Photoresistor on A0
+// install Photoresistor on
 int lightPin = A0;
+// install the 18B20 Temperature sensor on
+int exactTempPin = 7;
 
 
-
-
-
-// initialize the light variable
+// initialize the sensor variables
 int lightValue = 0;
 
+float exactTempValue = 0;
 
+OneWire oneWire(exactTempPin);
+DallasTemperature sensor(&oneWire);
 
 
 
 void setup() {
+    // Get a serial connection for reporting
     Serial.begin(9600);
+
+    // initialize the temp sensor
+    sensor.begin();
 }
 
 void loop() {
     // measure daylight
     lightValue = analogRead(lightPin);
 
+    // measure temp.
+    sensor.requestTemperatures();
+    exactTempValue = sensor.getTempCByIndex(0);
+
+    // All the serial output
     Serial.print("Light Value:");
     Serial.print(lightValue);
     Serial.println(";");
 
+    Serial.print("Current exact Temperature: ");
+    Serial.print(exactTempValue);
+    Serial.println("C;");
+
+
+    // sleep a little
     delay(timeout);
 }
 
