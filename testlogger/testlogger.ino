5d6a913798fd8ef900af54aec5e889abafb74ac5
Michael Lustenberger <mic@inofix.ch>
2017-08-22 14:10:21 +0200

    Digital Humidity Temperature Sensor
    Add the DHT sensor.

--- testlogger/testlogger.ino
+++ testlogger/testlogger.ino
@@ -1,6 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
+#include <DHT.h>
 
 // wait a second (or so..)
 int timeout = 5000;
@@ -10,16 +10,26 @@ int lightPin = A0;
 // install the 18B20 Temperature sensor on
 int exactTempPin = 7;
 
+// install Digital Humidity/Temperature sensor on
+int dhtPin = 6;
 
 // initialize the sensor variables
 int lightValue = 0;
 
+// 18B20
 float exactTempValue = 0;
 
+// DHT
+float envTempValue = 0;
+float envHumidityValue = 0;
+float heatIndex = 0;
+
+// 18B20
 OneWire oneWire(exactTempPin);
 DallasTemperature sensor(&oneWire);
 
-
+// DHT
+DHT dht(dhtPin, DHT11);
 
 void setup() {
     // Get a serial connection for reporting
@@ -27,6 +37,9 @@ void setup() {
 
     // initialize the temp sensor
     sensor.begin();
+
+    // initialize the DHT
+    dht.begin();
 }
 
 void loop() {
@@ -37,6 +50,15 @@ void loop() {
     sensor.requestTemperatures();
     exactTempValue = sensor.getTempCByIndex(0);
 
+    // measure humidity and temp.
+    envTempValue = dht.readTemperature();
+    envHumidityValue = dht.readHumidity();
+    if (isnan(envHumidityValue) || isnan(envTempValue)) {
+        Serial.println("Error reading environment data.");
+    } else {
+        heatIndex = dht.computeHeatIndex(envTempValue, envHumidityValue, false);
+    }
+
     // All the serial output
     Serial.print("Light Value:");
     Serial.print(lightValue);
@@ -46,6 +68,13 @@ void loop() {
     Serial.print(exactTempValue);
     Serial.println("C;");
 
+    Serial.print("Humidity: ");
+    Serial.print(envHumidityValue);
+    Serial.print("%; Temperature: ");
+    Serial.print(envTempValue);
+    Serial.print("C; Heat Index: ");
+    Serial.print(heatIndex);
+    Serial.println(";");
 
     // sleep a little
     delay(timeout);
