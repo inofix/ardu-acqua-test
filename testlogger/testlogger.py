5b1f19cf88d869121ac6184c286d837ad8651aac
Michael Lustenberger <mic@inofix.ch>
2017-09-19 20:15:40 +0200

    Python ArgParser
    Make the serial parameters flexible..

--- testlogger/testlogger.py
+++ testlogger/testlogger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import serial
 import sys
 
@@ -14,10 +15,15 @@ def serial_read(device, baudrate):
                 print data
 
     except:
-        print "Could not connect to the serial line at" + device + " " + \
-            baudrate
+        print "Could not connect to the serial line at " + device
 
 if __name__ == '__main__':
 
-    serial_read('/dev/ttyACM1', 9600)
+    cli_parser = argparse.ArgumentParser(description="Parse data from the arduino and use it for the Flussbad-Demo.")
+    cli_parser.add_argument('-d', '--device', default='/dev/ttyACM1', help='serial device the arduino is connected to')
+    cli_parser.add_argument('-b', '--baudrate', default=9600, help='baud rate of the serial line')
+    args = cli_parser.parse_args()
+
+
+    serial_read(args.device, args.baudrate)
 
