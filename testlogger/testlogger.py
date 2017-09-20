148cd96cd5157b4a603e6997e0039963183597a0
Michael Lustenberger <mic@inofix.ch>
2017-09-21 00:23:08 +0200

    Minor Change
    Start with the first device..

--- testlogger/testlogger.py
+++ testlogger/testlogger.py
@@ -203,7 +203,7 @@ if __name__ == '__main__':
     Main function used if started on the command line
     """
     cli_parser = argparse.ArgumentParser(description="Parse data from the arduino and use it for the Flussbad-Demo.")
-    cli_parser.add_argument('-d', '--device', default='/dev/ttyACM1', help='serial device the arduino is connected to')
+    cli_parser.add_argument('-d', '--device', default='/dev/ttyACM0', help='serial device the arduino is connected to')
     cli_parser.add_argument('-b', '--baudrate', default=9600, help='baud rate of the serial line')
     cli_parser.add_argument('-i', '--interactive', action="store_true", help='prompt for control')
     cli_parser.add_argument('-s', '--seconds', type=int, default=10, help='how long to run if not in interacitve mode')
