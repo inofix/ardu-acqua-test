6e944e3e3beabb30b75c133e04727d0ddcbb854f
Michael Lustenberger <mic@inofix.ch>
2017-09-20 22:36:29 +0200

    Number of Rounds
    Add a number of rounds to stop recording if reached. Alternative to the
    seconds parameter.

--- testlogger/testlogger.py
+++ testlogger/testlogger.py
@@ -53,14 +53,19 @@ class SerialReader(threading.Thread):
     Reader class for connecting to an end device and reading its output
     """
 
-    def __init__(self, device, baudrate, logger):
+    def __init__(self, device, baudrate, logger, rounds):
         """
         Initialize the serial reader class
+            device        device name to connect to
+            baudrate      the baud rate for the serial line
+            logger        the logger object to send the data to
+            rounds        number of rounds to run / listen for input
         """
         threading.Thread.__init__(self)
         self.device = device
         self.baudrate = baudrate
         self.logger = logger
+        self.rounds = rounds
         self.do_run = True
 
     def run(self):
@@ -84,6 +89,10 @@ class SerialReader(threading.Thread):
                         # now parse the input
                         data = data + "]"
                         self.logger.register_json(data)
+                        if self.rounds == 1:
+                            self.do_run = False
+                        elif self.rounds > 1:
+                            self.rounds -= 1
                     elif (l[0:3] == "  {"):
                         # this is a data line
                         data = data + " " + l
@@ -110,7 +119,11 @@ def user_mode(args):
     print "You have the following options:"
     print "    register [device] [baud]         add a device to observe"
     print "    unregister [device]              remove a device"
+    print "    rounds num                       number of rounds to run threads"
     print "    exit                             cleanup and quit"
+
+    # set the default number of rounds to run a thread from the CLI
+    rounds = args.rounds
 
     while True:
 
@@ -130,33 +143,9 @@ def user_mode(args):
         device_name = re.sub("/dev/", "", args.device)
         baudrate = args.baudrate
 
-        # if the device is specified, set it
-        if len(ms) > 1:
-            if ms[1][0:1] == "/":
-                device = ms[1]
-                device_name = ms[1].sub("/dev/", "", args.device)
-            else:
-                device = "/dev/" + ms[1]
-                device_name = ms[1]
-
-        # if device and baudrate are specified, also set the baudrate
-        if len(ms) > 2:
-            baudrate = ms[2]
-
-        # now do what the user wants
-        if (mode == "register"):
-            if threads.has_key(device_name):
-                print "This device was already registered"
-            else:
-                # create an object that connects to the serial line
-                threads[device_name] = SerialReader(device, baudrate, logger)
-                # start recording
-                threads[device_name].start()
-        elif (mode == "unregister"):
-            if threads.has_key(device_name) and isinstance(threads[device_name], SerialReader):
-                # end the recording and remove the device from the list
-                threads[device_name].halt()
-                threads.pop(device_name)
+        # now do what the user wants - part 1
+        if (mode == "rounds" and len(ms) > 1):
+            rounds = int(ms[1])
         elif (mode == "exit" or mode == "quit"):
             # clean up
             i = iter(threads)
@@ -166,8 +155,36 @@ def user_mode(args):
             # and exit
             return
         else:
-            print "This mode is not supported: " + mode
-            print "Use one of 'start', 'stop', or 'exit' ..."
+            # if the device is specified, set it
+            if len(ms) > 1:
+                if ms[1][0:1] == "/":
+                    device = ms[1]
+                    device_name = ms[1].sub("/dev/", "", args.device)
+                else:
+                    device = "/dev/" + ms[1]
+                    device_name = ms[1]
+
+            # if device and baudrate are specified, also set the baudrate
+            if len(ms) > 2:
+                baudrate = ms[2]
+
+            # now do what the user wants
+            if (mode == "register"):
+                if threads.has_key(device_name):
+                    print "This device was already registered"
+                else:
+                    # create an object that connects to the serial line
+                    threads[device_name] = SerialReader(device, baudrate, logger, rounds)
+                    # start recording
+                    threads[device_name].start()
+            elif (mode == "unregister"):
+                if threads.has_key(device_name) and isinstance(threads[device_name], SerialReader):
+                    # end the recording and remove the device from the list
+                    threads[device_name].halt()
+                    threads.pop(device_name)
+            else:
+                print "This mode is not supported: " + mode
+                print "Use one of 'start', 'stop', or 'exit' ..."
 
 def standard_mode(args):
     """
@@ -175,7 +192,7 @@ def standard_mode(args):
     """
     logger = DataLogger()
 
-    thread = SerialReader(args.device, args.baudrate, logger)
+    thread = SerialReader(args.device, args.baudrate, logger, args.rounds)
     thread.start()
     time.sleep(args.seconds)
     thread.halt()
@@ -189,6 +206,7 @@ if __name__ == '__main__':
     cli_parser.add_argument('-b', '--baudrate', default=9600, help='baud rate of the serial line')
     cli_parser.add_argument('-i', '--interactive', action="store_true", help='prompt for control')
     cli_parser.add_argument('-s', '--seconds', type=int, default=10, help='how long to run if not in interacitve mode')
+    cli_parser.add_argument('-r', '--rounds', type=int, default=0, help='how many times to run the serial listener thread (default: 0 / infinite)')
     args = cli_parser.parse_args()
 
     if args.interactive:
