06f66419203a479fd5f528daec904c75b69a5e0b
Michael Lustenberger <mic@inofix.ch>
2017-09-20 12:55:27 +0200

    Threading
    Create a separate thread for the serial input.

--- testlogger/testlogger.py
+++ testlogger/testlogger.py
@@ -4,41 +4,49 @@ import argparse
 import json
 import serial
 import sys
+import time
+import threading
 
-def serial_read(device, baudrate):
-    try:
-        arduino_serial = serial.Serial(device, baudrate);
+class serial_read(threading.Thread):
 
-        result = {}
-        data = ""
+    def __init__(self, device, baudrate):
+        threading.Thread.__init__(self)
+        self.device = device
+        self.baudrate = baudrate
 
-        while (True):
-            if (arduino_serial.inWaiting() > 1):
-                l = arduino_serial.readline()[:-2]
+    def run(self):
+        try:
+            arduino_serial = serial.Serial(self.device, self.baudrate);
 
-                if (l == "["):
-                    # start recording
-                    data = "["
-                elif (l == "]") and (len(data) > 4) and (data[0] == "["):
-                    print data
-                    # now parse the input
-                    data = data + "]"
-                    j = json.loads(data)
+            data = ""
+
+            while (True):
+                if (arduino_serial.inWaiting() > 1):
+                    l = arduino_serial.readline()[:-2]
+
+                    if (l == "["):
+                        # start recording
+                        data = "["
+                    elif (l == "]") and (len(data) > 4) and (data[0] == "["):
+                        print data
+                        # now parse the input
+                        data = data + "]"
+                        j = json.loads(data)
 ## TODO: debug start
-                    print ""
-                    for v in j:
-                        if v.has_key("unit"):
-                            u = " " + v["unit"]
-                        else:
-                            u = ""
-                        print v["name"] + " " + v["value"] + u
+                        print ""
+                        for v in j:
+                            if v.has_key("unit"):
+                                u = " " + v["unit"]
+                            else:
+                                u = ""
+                            print v["name"] + " " + v["value"] + u
 ## TODO: debug end
-                elif (l[0:3] == "  {"):
-                    # this is a data line
-                    data = data + " " + l
+                    elif (l[0:3] == "  {"):
+                        # this is a data line
+                        data = data + " " + l
 
-    except serial.serialutil.SerialException:
-        print "Could not connect to the serial line at " + device
+        except serial.serialutil.SerialException:
+            print "Could not connect to the serial line at " + device
 
 if __name__ == '__main__':
 
@@ -47,5 +55,6 @@ if __name__ == '__main__':
     cli_parser.add_argument('-b', '--baudrate', default=9600, help='baud rate of the serial line')
     args = cli_parser.parse_args()
 
-
-    serial_read(args.device, args.baudrate)
+#    serial_read(args.device, args.baudrate)
+    thread = serial_read(args.device, args.baudrate)
+    thread.start()
