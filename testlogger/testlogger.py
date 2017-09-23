ee551a380d34853bf3a429beb6d9e013961ae43f
Michael Lustenberger <mic@inofix.ch>
2017-09-23 21:16:03 +0200

    Minor Change
    Explain interactive mode.

--- testlogger/testlogger.py
+++ testlogger/testlogger.py
@@ -257,7 +257,7 @@ if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser(description="Parse data from the arduino and use it for the Flussbad-Demo.")
     cli_parser.add_argument('-b', '--baudrate', default=9600, help='baud rate of the serial line')
     cli_parser.add_argument('-d', '--device', default='/dev/ttyACM0', help='serial device the arduino is connected to')
-    cli_parser.add_argument('-i', '--interactive', action="store_true", help='prompt for control')
+    cli_parser.add_argument('-i', '--interactive', action="store_true", help='prompt for control and log to stdout')
     cli_parser.add_argument('-I', '--insecure', action="store_true", help='do not verify certificate on HTTPS POST')
     cli_parser.add_argument('-p', '--password', action="store_true", help='prompt for a password')
     cli_parser.add_argument('-P', '--password-file', default='', help='load password from this file, containing the line: \'password: "my secret text"\'')
